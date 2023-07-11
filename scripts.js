16673a3700107cfcd7fa1c732df90b9f6936b45f
Gul Noor Desktop <gulnoor5233@gmail.com>
2023-07-11 13:00:55 +0500

    added transition to navbar when scrolling window

--- scripts.js
+++ scripts.js
@@ -1,2 +1,22 @@
 const buy_now = document.getElementById("buy-now");
 buy_now.addEventListener("click", ()=>{window.document.location.href="booking-form.html"})
+
+const navbar = document.querySelector("nav");
+window.addEventListener("scroll",extend_nav)
+
+function extend_nav(){
+    nav_top = navbar.getBoundingClientRect().top
+    if (nav_top == 0){
+        navbar.style.marginLeft = "-5vw"
+        navbar.style.marginRight = "-5vw"
+        navbar.style.borderRadius = "0"
+        // navbar.style.animation = "extend_navbar 0.5s ease-in-out forwards"
+    }
+    else{
+        navbar.style.marginLeft = "0"
+        navbar.style.marginRight = "0"
+        navbar.style.borderRadius = "50vh"
+        // navbar.style.animation = "contract_navbar 0.5s ease-in-out forwards"
+    }
+
+}
