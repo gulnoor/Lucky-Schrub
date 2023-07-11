df8bf7e631920266eae5ca49b48d602a2df650b1
Gul Noor Desktop <gulnoor5233@gmail.com>
2023-07-11 15:20:48 +0500

    converted transition to animation

--- scripts.js
+++ scripts.js
@@ -2,21 +2,27 @@ const buy_now = document.getElementById("buy-now");
 buy_now.addEventListener("click", ()=>{window.document.location.href="booking-form.html"})
 
 const navbar = document.querySelector("nav");
-window.addEventListener("scroll",extend_nav)
+window.addEventListener("scroll",anim_nav)
 
-function extend_nav(){
+function anim_nav(){
     nav_top = navbar.getBoundingClientRect().top
+    computed_nav = getComputedStyle(navbar)
+    
+    cm = computed_nav.marginLeft
+    navbar.style.setProperty("--cnm",cm)
     if (nav_top == 0){
-        navbar.style.marginLeft = "-5vw"
-        navbar.style.marginRight = "-5vw"
-        navbar.style.borderRadius = "0"
-        // navbar.style.animation = "extend_navbar 0.5s ease-in-out forwards"
+        // navbar.style.marginLeft = "-5vw"
+        // navbar.style.marginRight = "-5vw"
+        // navbar.style.borderRadius = "0" 
+        navbar.style.animation = "extend_navbar 0.5s ease-in-out forwards"
     }
     else{
-        navbar.style.marginLeft = "0"
-        navbar.style.marginRight = "0"
-        navbar.style.borderRadius = "50vh"
-        // navbar.style.animation = "contract_navbar 0.5s ease-in-out forwards"
+        
+        // navbar.style.marginLeft = "0"
+        // navbar.style.marginRight = "0"
+        // navbar.style.borderRadius = "50vh"
+        
+        navbar.style.animation = "contract_navbar 0.5s ease-in-out forwards"
     }
 
 }
