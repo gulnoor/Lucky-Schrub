dd8e768fce149f4135644d99640f1044f729afea
Gul Noor Desktop <gulnoor5233@gmail.com>
2023-07-30 19:29:22 +0500

    ...

--- scripts.js
+++ scripts.js
@@ -6,20 +6,11 @@ window.addEventListener("scroll",anim_nav)
 
 function anim_nav(){
     nav_top = navbar.getBoundingClientRect().top
-    // computed_nav = getComputedStyle(navbar)
-    // cm = computed_nav.marginLeft
-    // navbar.style.setProperty("--cnm",cm)
     if (nav_top == 0){
-        navbar.style.marginLeft = "-5vw"
-        navbar.style.marginRight = "-5vw"
         navbar.style.borderRadius = "0" 
-        // navbar.style.animation = "extend_navbar 0.2s ease-in-out forwards"
     }
     else{
-        navbar.style.marginLeft = "0"
-        navbar.style.marginRight = "0"
         navbar.style.borderRadius = "50vh"
-        // navbar.style.animation = "contract_navbar 0.5s ease-in-out forwards"
     }
 
 }
