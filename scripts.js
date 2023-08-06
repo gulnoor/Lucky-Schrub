d3e84fd0d1992e2e9bfd9f6f32278be9bc079bf0
gulnoor <gulnoor5233@gmail.com>
2023-08-06 23:59:22 +0500

    redesigned navbar using mobile first approach

--- scripts.js
+++ scripts.js
@@ -1,16 +1,69 @@
+// (function(){
+    
+    "use strict";
+    const navbar = document.querySelector("nav");
+    const dropdown = document.querySelector(".dropnav-content");
+    const navbtn = navbar.querySelector(".dropnav-button");
+
+    window.addEventListener("scroll",anim_nav);
+    window.addEventListener("resize",windowResize);
+
+
+    navbtn.addEventListener("mouseenter",navHover);
+    navbar.addEventListener("mouseleave",navLeave);
+
+    navbtn.addEventListener("click",navClick)
+
+
+    function windowResize(){
+        if (window.innerWidth >= 600){
+            dropdown.style.maxHeight = "1000px";
+        }
+        else{
+            dropdown.style.maxHeight = "0px";
+        }
+    }
+    function navClick(){
+        let maxh = window.getComputedStyle(dropdown).maxHeight;
+
+        if (maxh !== "0px") {
+            dropdown.style.maxHeight = "0px";
+            navbtn.innerHTML = "MENU";
+        }
+        else{dropdown.style.maxHeight = "1000px" ;
+        navbtn.innerHTML = "&#x25BC;";}
+            
+    }
+    function navHover(){
+            navbtn.innerHTML = "&#x25BC;";
+            dropdown.style.maxHeight = "1000px";
+        
+
+    }
+    function navLeave(){
+        
+        navbtn.innerHTML = "MENU";
+        if (window.innerWidth < 600){
+            dropdown.style.maxHeight = "0px";
+        }
+    }
+    function anim_nav(){
+        let nav_top = navbar.getBoundingClientRect().top;
+        if ( nav_top === 0){
+            navbar.style.borderRadius = "0"
+            navbar.style.marginLeft = "0" 
+            navbar.style.marginRight = "0"
+        }
+        else{
+            navbar.style.borderRadius = "1rem"
+            navbar.style.marginLeft = "1rem"
+            navbar.style.marginRight = "1rem"
+        }
+        
+    }
+    
+// }());
+
+const mainContent = document.querySelector(".main-content");
 const buy_now = document.getElementById("buy-now");
-buy_now.addEventListener("click", ()=>window.document.location.href="booking-form.html")
-
-const navbar = document.querySelector("nav");
-// window.addEventListener("scroll",anim_nav)
-
-function anim_nav(){
-    nav_top = navbar.getBoundingClientRect().top
-    if (nav_top == 0){
-        navbar.style.borderRadius = "0" 
-    }
-    else{
-        navbar.style.borderRadius = "50vh"
-    }
-
-}
+buy_now.addEventListener("click", ()=>mainContent.innerHTML="hello")
