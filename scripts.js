564367d9714118d41b7b00f9d608e41dd7d1c39e
gulnoor <gulnoor5233@gmail.com>
2023-08-09 01:06:36 +0500

    added event listeners to navbar

--- scripts.js
+++ scripts.js
@@ -1,69 +1,62 @@
-// (function(){
-    
-    "use strict";
-    const navbar = document.querySelector("nav");
-    const dropdown = document.querySelector(".dropnav-content");
-    const navbtn = navbar.querySelector(".dropnav-button");
+(function () {
+  "use strict";
+  const navbar = document.querySelector("nav");
+  const dropdown = document.querySelector(".dropnav-content");
+  const navbtn = navbar.querySelector(".dropnav-button");
 
-    window.addEventListener("scroll",anim_nav);
-    window.addEventListener("resize",windowResize);
+  window.addEventListener("scroll", anim_nav);
+  window.addEventListener("resize", windowResize);
+  navbar.addEventListener("mouseenter", navHover);
+  navbar.addEventListener("mouseleave", navLeave);
+  navbar.addEventListener("click", navClick);
 
-
-    navbtn.addEventListener("mouseenter",navHover);
-    navbar.addEventListener("mouseleave",navLeave);
-
-    navbtn.addEventListener("click",navClick)
-
-
-    function windowResize(){
-        if (window.innerWidth >= 600){
-            dropdown.style.maxHeight = "1000px";
-        }
-        else{
-            dropdown.style.maxHeight = "0px";
-        }
+  function windowResize() {
+    if (window.innerWidth >= 600) {
+      dropdown.style.maxHeight = "1000px";
+    } else {
+      dropdown.style.maxHeight = "0px";
     }
-    function navClick(){
-        let maxh = window.getComputedStyle(dropdown).maxHeight;
+  }
+  function navClick() {
+    let maxh = window.getComputedStyle(dropdown).maxHeight;
+    if (maxh !== "0px") {
+      dropdown.style.maxHeight = "0px";
+      navbtn.innerHTML = "☰ MENU";
+      
 
-        if (maxh !== "0px") {
-            dropdown.style.maxHeight = "0px";
-            navbtn.innerHTML = "MENU";
-        }
-        else{dropdown.style.maxHeight = "1000px" ;
-        navbtn.innerHTML = "&#x25BC;";}
-            
-    }
-    function navHover(){
-            navbtn.innerHTML = "&#x25BC;";
-            dropdown.style.maxHeight = "1000px";
-        
+    } else {
+      dropdown.style.maxHeight = "1000px";
+      navbtn.innerHTML = "&#x25BC;";
+      
+
 
     }
-    function navLeave(){
-        
-        navbtn.innerHTML = "MENU";
-        if (window.innerWidth < 600){
-            dropdown.style.maxHeight = "0px";
-        }
-    }
-    function anim_nav(){
-        let nav_top = navbar.getBoundingClientRect().top;
-        if ( nav_top === 0){
-            navbar.style.borderRadius = "0"
-            navbar.style.marginLeft = "0" 
-            navbar.style.marginRight = "0"
-        }
-        else{
-            navbar.style.borderRadius = "1rem"
-            navbar.style.marginLeft = "1rem"
-            navbar.style.marginRight = "1rem"
-        }
-        
-    }
-    
-// }());
+  }
 
-const mainContent = document.querySelector(".main-content");
-const buy_now = document.getElementById("buy-now");
-buy_now.addEventListener("click", ()=>mainContent.innerHTML="hello")
+  function navHover() {
+    navbtn.innerHTML = "&#x25BC;";
+    dropdown.style.maxHeight = "1000px";
+  }
+  function navLeave() {
+    navbtn.innerHTML = "☰ MENU";
+    if (window.innerWidth < 600) {
+      dropdown.style.maxHeight = "0px";
+    }
+  }
+  function anim_nav() {
+    let nav_top = navbar.getBoundingClientRect().top;
+    if (nav_top === 0) {
+      navbar.style.borderRadius = "0";
+      navbar.style.marginLeft = "0";
+      navbar.style.marginRight = "0";
+    } else {
+      navbar.style.borderRadius = "1rem";
+      navbar.style.marginLeft = "1rem";
+      navbar.style.marginRight = "1rem";
+    }
+  }
+
+  const mainContent = document.querySelector(".main-content");
+  const buy_now = document.getElementById("buy-now");
+  buy_now.addEventListener("click", () => (mainContent.innerHTML = "hello"));
+})();
